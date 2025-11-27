e40c0daebf8679bc00d41044f79d60312dc1f67a
yasum <yasumu@nifty.com>
2025-11-27 17:42:44 +0900

    refactor: インポートの最適化

--- feature/src/main/java/com/yasumu/feature/stock/StockListViewModel.kt
+++ feature/src/main/java/com/yasumu/feature/stock/StockListViewModel.kt
@@ -3,15 +3,14 @@ package com.yasumu.feature.stock
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.yasumu.core.data.fake.FakeStockRepository
 import com.yasumu.core.domain.model.Stock
+import com.yasumu.core.domain.repository.StockRepository
 import com.yasumu.core.domain.usecase.GetStockListUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
-import com.yasumu.core.domain.repository.StockRepository
 
 data class StockListUiState(
     val isLoading: Boolean = true,
