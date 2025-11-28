0767a48634e8067d5cb895c12fd6e2193757f63d
yasum <yasumu@nifty.com>
2025-11-28 11:05:47 +0900

    refactor: UIのパッケージ名stock をstock_listに変更

--- app/src/main/java/com/yasumu/freezerman/navigation/FreezerManNavHost.kt
+++ app/src/main/java/com/yasumu/freezerman/navigation/FreezerManNavHost.kt
@@ -5,7 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.yasumu.core.domain.repository.StockRepository
-import com.yasumu.feature.stock.StockListRoute
+import com.yasumu.feature.stock_list.StockListRoute
 
 
 @Composable
